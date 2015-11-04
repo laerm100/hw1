611622456d81ec3877899874e0ead028720a2175
laerm100 <Larissa.Ermoschkin@hhu.de>
2015-11-04 21:52:11 +0000

    Update fibonacci.cxx
    hw1 endlich fertig

--- fibonacci.cxx
+++ fibonacci.cxx
@@ -1,1 +1,27 @@
 
+#include<iostream>
+
+using namespace std;
+int main () {
+    int N;
+cout <<"Bitte gebe N ein: N="<< endl;
+cin >> N;
+
+int i, fi, f0, f1;
+ 
+f0=0;
+f1=1;
+    if (N==0) {
+	fi=f0;
+	}
+    if (N==1) {
+	fi=f1;
+	}
+    else for (i=1; i<N; i++) {
+	fi=f1+f0;
+	f0=f1;
+	f1=fi;
+	}
+    cout<<"Das Ergebnis lautet: fi=" <<fi<< endl;
+    return 0;
+    }
